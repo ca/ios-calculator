276ec68485144c27aba850b62113b3900e35981b
Alex Vallorosi <alexvallorosi@gmail.com>
2016-09-14 21:00:11 -0400

    initial commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DamopolousLabel: UILabel!
+    
+    @IBAction func DamopolousButton(sender: AnyObject) {
+        DamopolousLabel.text = "YOU CLICKED MY BUTTON"
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var CalculatorOutputLabel: UILabel!
+    var runningTotal:Double = 0
+    
+    @IBAction func OneButtonAction(sender: UIButton) {
+        if let textValue = sender.titleLabel?.text {
+            let value = (textValue as NSString).doubleValue
+            runningTotal = value
+            CalculatorOutputLabel.text = "\(runningTotal)"
+        }
+    }
+    
+    @IBAction func AddButtonAction(sender: AnyObject) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        DamopolousLabel.text = "TESTING"
     }
 
     override func didReceiveMemoryWarning() {
